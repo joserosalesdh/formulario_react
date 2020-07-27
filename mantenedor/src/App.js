0e56a843419c758081e3cc39ddd190a9ae049d66
joserosalesdh <jm.rosales92@gmail.com>
2020-07-27 14:20:38 -0300

    commit

--- mantenedor/src/App.js
+++ mantenedor/src/App.js
@@ -22,11 +22,37 @@ class App extends Component {
       // Este atributo de usuario seleccionado lo vamos a agregar al array vacio de data en el state y lo vamos a utilizar cuando nos encontramos en la vista d euserForm
     })
   }
+
+  agregarNuevoUsuario = usuario => {
+    // Vamos a simular un llamado a la API con un axios 
+    axios.post('https://jsonplaceholder.typicode.com/users', usuario)
+    console.log(usuario)
+    .then(({data}) => {
+      const newData =  this.state.data.concat(data) //Todos los datos que se encuetra dentro de la propiedad del estado y lo vamos a concatenar con data
+      this.setState({
+        data: newData,
+        ruta: 'lista'
+      })
+    })
+  }
+
+  actualizarNuevoUsuario = (id, values) => { // Cuando tomo el id voy a construir la url a la cual yo tengo que enviarle los datos para actualizar el recursos y le voy a enviar los nuevos valores que necesita para actualizar y uso axios.put
+    axios.put(`https://jsonplaceholder.typicode.com/users/$(id)`, values) // Uso en vez de comillas simple Template String me van a servir para construir string dinamicos, esto quiere decir que van a ir cambiando dependiendo d elos valore que yo le asigne  
+    .then(()=> {
+      const newData = this.state.data.map(x => x.id === id ? values :x) // Si encontramos el elemento aca lo que vamos a hacer es remplazarlo por lo que viene en values
+    this.setState({
+      data: newData,
+      ruta: 'lista'
+    })
+    })
+  }
+
   nuevoUsuario = () =>{
-    this.setState({ruta: 'formulario'})
+    this.setState({ruta: 'formulario', usuarioSeleccionado: undefined})
   }
   render(){
-    const {ruta, data} = this.state
+    const {ruta, data, usuarioSeleccionado} = this.state
+    const valoresIniciales = usuarioSeleccionado && data.find(x => x.id === usuarioSeleccionado) //Si usuario seleccionado se encuentra definido
     return (
       <div className="App">
         {/* Los datos que vamos a recibir de nuestra APi se lo tenemos que pasar a nuestro ViewList */}
@@ -35,7 +61,11 @@ class App extends Component {
         handleClick={this.seleccionaUsuario} 
         data={data}
         />}
-        {ruta === 'formulario' && <UserForm />}
+        {ruta === 'formulario' && <UserForm 
+        valoresIniciales={valoresIniciales || {}} // Si valoresIniciales no esta definido en ese caso quiero que tenga el valor d eun objeto, de esta manera en el forma vamos a poder preguntar por las propiedades de los valores iniciales y si es que se encuentra indefinido vamos a poder preguntar de todas maneras por las propiedades xq se nos va a devolver un objeto que va a tener sus propiedades indefinidas, pero lo que va a hacer es devolvernos undefined en lugar de crashear
+        handleSubmit={this.agregarNuevoUsuario}
+        handleUpdate={this.actualizarNuevoUsuario}
+        />}
       </div>
     );
   }
--- mantenedor/src/components/UserForm.jsx
+++ mantenedor/src/components/UserForm.jsx
@@ -1,9 +1,70 @@
 import React, {Component} from 'react';
 
+
+
+const validate = values => {
+    const errors = {}
+    if (!values.name) {
+        errors.name = 'Este campo es obligatorio'
+    }
+    if (!values.email) {
+        errors.email = 'Este campo es obligatorio'
+    }
+    if (!values.wesite) {
+        errors.website = 'Este campo es obligatorio'
+    }
+        return errors
+}
 class UserForm extends Component {
+    state = {
+        errors:{}
+    }
+
+    constructor(props){
+        super(props)
+        this.state = {
+            ...this.state,
+            ...props.valoresIniciales //dentro de las propiedades vamos a sacar sus valores iniciales
+        }
+    }
+    handleChange = ({target}) => { // Saco la propiedad target del evento e con destructuring
+        this.setState({
+            [target.name]: target.value, //a la propiedad dinamica target.name le vamos a asignar target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.prevenetDefault()
+        const {errors, ...sinErrors} = this.state
+        const result = validate(sinErrors)
+        
+        // Hago un if para validar de que el formulario no tenga ningun error, en la logica dice que si existe por lo menos un error se va a evaluar en tru y con la negacion en falso
+        if(!Object.keys(result).length){
+            const {handleSubmit, handleUpdate, valoresIniciales} = this.props
+            if (valoresIniciales.id) { //Si tiene un id
+                handleUpdate(valoresIniciales.id, sinErrors)
+            }else{ //En el caso de que no tenga un id
+                handleSubmit(sinErrors)
+            }
+            //Envio del formulario
+            //e.target.reset() no es necesario, agrego un else abajo para que de si es que llegase a existir un error en ese caso setea el estado
+        }else{
+            this.setState({errors: result}) 
+        }
+    }
     render(){
+        const {errors} = this.state
+        const {valoresIniciales} = this.props // valoresIniciales va a contar con un id que se lo vamos a pasar cuando s ellame al evento de handleSubmit 
         return(
-            'vista de formulario'
+            <form onSubmit={this.handleSubmit}>
+                <input defaultValue={valoresIniciales.name} placeholder="Nombre" type="text" name="name" onChange={this.handleChange}/>
+                {errors.name && <p>{errors.name}</p>}
+                <input defaultValue={valoresIniciales.email} placeholder="Email" type="text" name="email" onChange={this.handleChange}/>
+                {errors.email && <p>{errors.email}</p>}
+                <input defaultValue={valoresIniciales.website} placeholder="Sitio Web" type="text" name="website" onChange={this.handleChange}/>
+                {errors.website && <p>{errors.website}</p>}
+                <input type="submit" value="Enviar"/>
+            </form>
         )
     }
 }
